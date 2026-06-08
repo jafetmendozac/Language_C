8f75348ef9832eac485c7ee3c05578c1dcac7b55
jafetmendozac <abrahamjoelmc@gmail.com>
2026-06-08 09:09:01 -0500

    feat: 1.5.2 - 1.5.3

--- chapter_01/1.5_input_output_characters.c
+++ chapter_01/1.5_input_output_characters.c
@@ -2,15 +2,56 @@
 
 /* 1.5.1 Copy files */
 
+// int main() {
+//   int c;
+
+//   c=getchar();
+
+//   while(c!= EOF){ //EOF =-1
+//     putchar(c);
+//     c=getchar();  
+//   }
+// }
+
+
+/* 1.5.2 Count Characters*/
+
+// int main() {
+//   long nc;
+
+//   nc=0;
+
+//   while(getchar()){
+//     ++nc;
+//     printf("%ld\n", nc);
+//   }
+
+//   return 0;
+// }
+
+// int main() {
+//   double nc;
+
+//   for(nc=0; getchar()!= EOF; ++nc)
+//     printf("%.0f\n", nc);
+
+//   return 0;
+// }
+
+/* Count Lines */
+
 int main() {
-  int c;
+  int c, nl;
 
-  c=getchar();
+  nl=0;
 
-  while(c!= EOF){ //EOF =-1
-    putchar(c);
-    c=getchar();  
+  while((c = getchar())!= EOF){
+    if(c=='\n')
+      ++nl;
+    printf("%d\n", nl);
   }
 
+  return 0;
 }
-
+//Un carácter escrito entre apóstrofos representa un valor entero igual al valor
+//numérico del carácter en el conjunto de caracteres de la máquina
